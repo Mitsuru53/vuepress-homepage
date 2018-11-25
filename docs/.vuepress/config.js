880389edfac4bceb742f9505a4cf83eaf571c8c6
mitsuruhashimoto <exhikkii@gmail.com>
2018-11-25 14:42:32 +0900

    initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'About', link: '/about/' },
+            {
+                text: 'SNS',
+                items: [
+                    { text: 'Facebook', link: 'https://www.facebook.com/mitsuru.hashimoto.75' },
+                    { text: 'Instagram', link: 'https://www.instagram.com/exhikkii/' },
+                    { text: 'GitHub', link: 'https://github.com/Mitsuru53' },
+                    { text: 'blog', link: 'https://exhikkii.hatenablog.com' },
+                    { text: 'blog', link: 'https://exhikkii.hatenablog.com' },
+                    { text: 'blog', link: 'https://exhikkii.hatenablog.com' },
+                    { text: 'blog', link: 'https://exhikkii.hatenablog.com' }
+                ]
+            }
+        ]
+    }
+}
